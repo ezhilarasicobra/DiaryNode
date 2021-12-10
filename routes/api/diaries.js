660f07fb086ccb1596903994c2dc6118dc5a0d42
Ezhilarasi <tezzhilarasi@gmail.com>
2021-12-10 12:51:25 +0530

    first comit

---
+++ routes/api/diaries.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const router = express.Router()
+const auth = require('../../middleware/auth')
+
+
+const Diary = require('../../models/Diary')
+
+
+router.get('/', auth, (req, res) => {
+  Diary.find({ user: req.user.id })
+    .sort({ date: -1 })
+    .then(diaries => res.json(diaries))
+    .catch(err => res.status(400).json(err))
+})
+
+
+router.get('/:id/view', auth, (req, res) => {
+  Diary.findById(req.params.id)
+    .then(diary => res.json(diary))
+    .catch(err => res.status(404).json({ success: false, message: err }))
+})
+
+
+router.post('/new', auth, (req, res) => {
+  const newDiary = new Diary({
+    title: req.body.title,
+    body: req.body.body,
+    user: req.user.id
+  })
+  newDiary
+    .save()
+    .then(diary => res.json(diary))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route Update api/diary
+// @desc Update A Diary
+// @access Private
+router.put('/:id/update', auth, (req, res) => {
+  Diary.findByIdAndUpdate(req.params.id, req.body)
+    .then(diary => res.json(diary))
+    .catch(err => res.status(400).json(err))
+})
+
+// @route Delete api/diaries
+// @desc Delete A Diary
+// @access Public
+router.delete('/:id/delete', auth, (req, res) => {
+  Diary.findById(req.params.id)
+    .then(diary => {
+      diary.remove().then(() => res.json({ success: true }))
+    })
+    .catch(err =>
+      res
+        .status(404)
+        .json({ success: false, message: 'Item does not exist', error: err })
+    )
+})
+
+module.exports = router
---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const morgan = require('morgan')
+const cors = require('cors')
+const path = require('path')
+const config = require('./config/keys')
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(morgan('tiny'))
+
+// Routes
+app.use('/api/diaries', require('./routes/api/diaries'))
+app.use('/api/users', require('./routes/api/users'))
+app.use('/api/auth', require('./routes/api/auth'))
+
+// Database
+const dbURI = config.MONGODB_URI
+mongoose
+  .connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  })
+  .then(() => {
+    console.log('Mongoose is connected.')
+  })
+  .catch(err => console.log(err))
+
+
+const PORT = process.env.PORT || 8080
+app.listen(PORT, console.log(`Server is running at port: ${PORT}`))
